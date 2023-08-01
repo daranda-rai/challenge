ea47c823c795398d9d87a9e379b619d2352af02a
daranda-rai <dalaranda@gmail.com>
2023-08-01 20:52:56 +0100

    info update

--- api/methods.js
+++ api/methods.js
@@ -1,6 +1,8 @@
 const { expect, request } = require("@playwright/test")
 
 module.exports = class Methods {
+
+	// GET Request with default headers, only receives url and returns response json
 	async getData(apiUrl) {
 		const context = await request.newContext()
 		console.log(apiUrl)
--- tests/specs/test.spec.js
+++ tests/specs/test.spec.js
@@ -13,10 +13,15 @@ test.afterEach(async({}, testInfo) => {
 	console.log ("### Finished test '" + testInfo.title + "' with status: " + testInfo.status)
 }) 
 test("Get daily covid cases in the uk", async({}, testInfo) => {
+
+	//GET covid data with structure and filter defined in the .env file
 	const responseData = await covidApi.getCovidCasesWithFilters()
+
+	// Gets the last date and case number
 	const date = await responseData.data[0].date
 	const newCases = await responseData.data[0].newCases
 
+	// Publishes the date and cases in a simple txt file on testEvidence folder
 	let data = [ date, newCases ]
 	utils.saveEvidenceTxt(testInfo.title, data)
 })
